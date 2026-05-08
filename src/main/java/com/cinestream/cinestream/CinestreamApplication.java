46c024d47fbea417ac51b282e238cb515295f931
viozkyyy <rionaldotriandaka@gmail.com>
2026-05-08 15:31:43 +0700

    Initial Spring Boot setup

---
+++ src/main/java/com/cinestream/cinestream/CinestreamApplication.java
@@ -0,0 +1,12 @@
+package com.cinestream.cinestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinestreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinestreamApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cinestream/cinestream/CinestreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinestream.cinestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinestreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
